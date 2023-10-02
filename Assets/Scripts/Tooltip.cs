37454bdb1dad08802fd42cd295b47e9d77158b2c
enderdove <golubinskiivladimir@gmail.com>
2023-10-02 16:00:49 +0300

    Tooltips

---
+++ Assets/Scripts/Tooltip.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+
+namespace Game
+{
+    [RequireComponent(typeof( Collider2D))]
+    public class Tooltip : MonoBehaviour
+    {
+        public Canvas canvas;
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (!collision.CompareTag("Player"))
+                return;
+
+            canvas.gameObject.SetActive(true);
+        }
+
+        private void OnTriggerExit2D(Collider2D collision)
+        {
+            if (!collision.CompareTag("Player"))
+                return;
+
+            canvas.gameObject.SetActive(false);
+        }
+    }
+}
